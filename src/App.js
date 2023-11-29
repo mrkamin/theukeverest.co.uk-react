c5f44cbb1f4b247a1fc685129893a8f2b2fce520
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-29 19:50:18 +0500

    Complete the Nav Uper Head

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import './App.css';
-import Header from './components/Header/Header';
+import { BrowserRouter as Router } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Header/NavUperHead.js
+++ src/components/Header/NavUperHead.js
@@ -1,14 +1,56 @@
+import { Link } from "react-router-dom";
 import StarSvg from "../StarSvg";
+import { FaFacebook } from "react-icons/fa";
+import { ImInstagram } from "react-icons/im";
 
 const NavUperHead = () => {
   return (
     <>
-      <div className="d-flex align-items-center gap-1">
+      <div className="d-flex align-items-center gap-2 p-1 justify-content-end">
         {" "}
         <div className="d-flex align-items-center">
           <StarSvg />
         </div>
         <p className="p-0 m-0">Review</p>
+        <ul className="list-group list-group-horizontal d-flex align-itmes-center p-0 m-0">
+          <li className="list-group-item">
+            <Link to="/aboutus" className="list-group-item-action active">
+              About Us
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/testimonials" className="list-group-item-action active">
+              Testimonials
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/blog" className="list-group-item-action active">
+              Blog
+            </Link>
+          </li>
+          <li className="list-group-item">
+            <Link to="/contactus" className="list-group-item-action active">
+              Contact Us
+            </Link>
+          </li>
+        </ul>
+        <p className="p-0 m-0">0797578699</p>
+        <div className="footer-socials">
+          <a
+            href="https://www.facebook.com/mrkamin2"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.instagram.com/rafikambiz/?hl=en"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <ImInstagram />
+          </a>
+        </div>
       </div>
     </>
   );
