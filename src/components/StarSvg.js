985e5f429f15d3458a64a455fdaa77e65089d3c6
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-11-29 18:16:30 +0500

    Add StarSvg component

---
+++ src/components/StarSvg.js
@@ -0,0 +1,63 @@
+const StarSvg = () => {
+  return (
+    <>
+      <svg
+        width="20"
+        height="20"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <polygon
+          points="10,1 12,6 17,6.5 13,10 14,15 10,12 6,15 7,10.5 3,6.5 8,6"
+          fill="#1f1f38"
+        />
+      </svg>
+      <svg
+        width="20"
+        height="20"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <polygon
+          points="10,1 12,6 17,6.5 13,10 14,15 10,12 6,15 7,10.5 3,6.5 8,6"
+          fill="#1f1f38"
+        />
+      </svg>
+      <svg
+        width="20"
+        height="20"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <polygon
+          points="10,1 12,6 17,6.5 13,10 14,15 10,12 6,15 7,10.5 3,6.5 8,6"
+          fill="#1f1f38"
+        />
+      </svg>
+      <svg
+        width="20"
+        height="20"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <polygon
+          points="10,1 12,6 17,6.5 13,10 14,15 10,12 6,15 7,10.5 3,6.5 8,6"
+          fill="#1f1f38"
+        />
+      </svg>
+      <svg
+        width="20"
+        height="20"
+        xmlns="http://www.w3.org/2000/svg"
+        className=""
+      >
+        <polygon
+          points="10,1 12,6 17,6.5 13,10 14,15 10,12 6,15 7,10.5 3,6.5 8,6"
+          fill="#1f1f38"
+        />
+      </svg>
+    </>
+  );
+};
+
+export default StarSvg;
