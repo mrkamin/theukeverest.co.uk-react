41df1b5083c7ca32744fc3493ce237f6a775e756
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-12-01 12:41:13 +0500

    Add Carts and Make responsive

--- src/components/hero/Home.js
+++ src/components/hero/Home.js
@@ -1,5 +1,8 @@
 import { Link } from "react-router-dom";
 import IMG from "../assets/banner1.jpg";
+import AIRCONDITIONIMG from "../assets/air conditioning.png";
+import ACOUTDOOR from '../assets/outdoor unit ac.jpg'
+import { GiAutoRepair } from "react-icons/gi";
 import "./home.css";
 
 const Home = () => {
@@ -11,7 +14,9 @@ const Home = () => {
       </div>
       <div className="banner-msg d-flex flex-column position-absolute align-items-center">
         <h1>Air Conditioning & Refrigeration Europe</h1>
-        <h5>Specialist Aircon Installation & Commercial Refrigeration Service</h5>
+        <h5>
+          Specialist Aircon Installation & Commercial Refrigeration Service
+        </h5>
         <div className="d-flex gap-2 mt-4">
           <Link
             to="our-projects"
@@ -26,6 +31,39 @@ const Home = () => {
             DISCUSS YOUER PROJECTS
           </Link>
         </div>
+      </div>
+      <div className="cart-container gap-3 w-100 p-3">
+        <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
+        <div className="cart-item align-items-center">
+        <img src={AIRCONDITIONIMG} alt="air" height={50} className="cart-icon"/>
+        <div className="text-start">
+          <h3 className="p-0 m-0">Air Conditioning Europe</h3>
+          <p className="p-0 m-0">Specialist Design and Installation</p>
+        </div>
+        </div>
+        <Link to="air-conditioning" className="read-more p-2 rounded-5">Read more</Link>
+      </div>
+      <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
+        <div className="cart-item align-items-center">
+        <img src={ACOUTDOOR} alt="air" height={50} className="cart-icon"/>
+        <div className="text-start">
+          <h3 className="p-0 m-0">Commercial Refrigeration</h3>
+          <p className="p-0 m-0">Walk-in Cold Rooms, Fridges & Freezers</p>
+        </div>
+        </div>
+        <Link to="commercial-refrigeration" className="read-more p-2 rounded-5">Read more</Link>
+      </div>
+      <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
+        <div className="cart-item align-items-center">
+        <GiAutoRepair />
+        <div className="text-start">
+          <h3 className="p-0 m-0">Repair and Maintenance</h3>
+          <p className="p-0 m-0">Air-conditioning and Refrigeration Systems</p>
+        </div>
+        </div>
+        <Link to="repair-maintenance" className="read-more p-2 rounded-5">Read more</Link>
+      </div>
+     
       </div>
     </div>
   );
