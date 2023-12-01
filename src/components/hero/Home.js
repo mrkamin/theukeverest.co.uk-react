a1ca73e275ec3b400ca66f27d50a48cdaf3bf36e
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-12-01 07:47:50 +0500

    Add Banner Image

--- src/components/hero/Home.js
+++ src/components/hero/Home.js
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
-import IMG from "../assets/The-Uk-Everest-banner.jpg";
+import IMG from "../assets/banner1.jpg";
 import "./home.css";
 
 const Home = () => {
   return (
     <div className="home position-relative">
-      <img src={IMG} alt="banner" className="banner-img" />
+      <div className="banner-container position-relative">
+        <img src={IMG} alt="banner" className="banner-img" />
+        <div className="overlay" />
+      </div>
       <div className="banner-msg d-flex flex-column position-absolute align-items-center">
         <h1>Air Conditioning & Refrigeration Europe</h1>
-        <p>Specialist Aircon Installation & Commercial Refrigeration Service</p>
-        <div className="d-flex gap-2">
-          <Link to="our-projects" className="border border-3 rounded-5 text-white p-2">OUR PROJECTS</Link>
-          <Link to="our-projects" className="border border-3 rounded-5 text-white p-2">DISCUSS YOUER PROJECTS</Link>
+        <h5>Specialist Aircon Installation & Commercial Refrigeration Service</h5>
+        <div className="d-flex gap-2 mt-4">
+          <Link
+            to="our-projects"
+            className="border border-3 rounded-5 text-dark p-2"
+          >
+            OUR PROJECTS
+          </Link>
+          <Link
+            to="our-projects"
+            className="border border-3 rounded-5 text-dark p-2"
+          >
+            DISCUSS YOUER PROJECTS
+          </Link>
         </div>
       </div>
     </div>
