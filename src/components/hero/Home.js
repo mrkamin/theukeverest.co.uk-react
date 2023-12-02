7c4dcdc45331f0b573baddbbb2bcb8ab1aa76b7f
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-12-02 19:02:00 +0500

    Make the app responsive

--- src/components/hero/Home.js
+++ src/components/hero/Home.js
@@ -1,10 +1,7 @@
 import { Link } from "react-router-dom";
 import IMG from "../assets/banner1.jpg";
-import AIRCONDITIONIMG from "../assets/air conditioning.png";
-import ACOUTDOOR from "../assets/outdoor unit ac.jpg";
-import { GiAutoRepair } from "react-icons/gi";
 import "./home.css";
-import HomeSectionThree from "./HomeSectionThree";
+import Cart from "./Cart";
 
 const Home = () => {
   return (
@@ -35,55 +32,7 @@ const Home = () => {
           </Link>
         </div>
       </div>
-      <div className="cart-container gap-3 w-100 p-3">
-        <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
-          <div className="cart-item align-items-center justify-content-center text-center">
-            <img
-              src={AIRCONDITIONIMG}
-              alt="air"
-              height={50}
-              className="cart-icon"
-            />
-            <div className="text-start">
-              <h3 className="p-0 m-0">Air Conditioning Europe</h3>
-              <p className="p-0 m-0">Specialist Design and Installation</p>
-            </div>
-          </div>
-          <Link to="air-conditioning" className="read-more p-2 rounded-5">
-            Read more
-          </Link>
-        </div>
-        <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
-          <div className="cart-item align-items-center">
-            <img src={ACOUTDOOR} alt="air" height={50} className="cart-icon" />
-            <div className="text-start">
-              <h3 className="p-0 m-0">Commercial Refrigeration</h3>
-              <p className="p-0 m-0">Walk-in Cold Rooms, Fridges & Freezers</p>
-            </div>
-          </div>
-          <Link
-            to="commercial-refrigeration"
-            className="read-more p-2 rounded-5"
-          >
-            Read more
-          </Link>
-        </div>
-        <div className="cart-items w-100 d-flex flex-column text-center p-5 gap-3">
-          <div className="cart-item align-items-center">
-            <GiAutoRepair />
-            <div className="text-start">
-              <h3 className="p-0 m-0">Repair and Maintenance</h3>
-              <p className="p-0 m-0">
-                Air-conditioning and Refrigeration Systems
-              </p>
-            </div>
-          </div>
-          <Link to="repair-maintenance" className="read-more p-2 rounded-5">
-            Read more
-          </Link>
-        </div>
-      </div>
-      <HomeSectionThree />
+      <Cart />
     </div>
   );
 };
