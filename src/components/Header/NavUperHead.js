0236db275a1e2373a7c53ac9cd8ddfd317f0d914
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-12-08 11:26:14 +0500

    Add HndleNavbarToggle func

--- src/components/Header/NavUperHead.js
+++ src/components/Header/NavUperHead.js
@@ -1,8 +1,14 @@
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import StarSvg from "./StarSvg";
 import { FaFacebook } from "react-icons/fa";
 
 const NavUperHead = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const closeNavbar = () => {
+    setNavbarOpen(false);
+  };
   return (
     <>
       <div className="d-flex justify-content-end">
@@ -10,7 +16,7 @@ const NavUperHead = () => {
         <nav className="navbar navbar-expand-lg p-0 m-0">
           <div className="container-fluid">
             <div
-              className="collapse navbar-collapse"
+              className={`collapse navbar-collapse ${navbarOpen ? "show" : ""}`}
               id="navbarSupportedContent"
             >
               <div className="d-flex align-items-center review">
@@ -19,43 +25,56 @@ const NavUperHead = () => {
               </div>
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <Link
+                  <NavLink
                     className="nav-link  fw-medium"
                     aria-current="page"
                     to="about-us"
+                    onClick={closeNavbar}
                   >
                     About Us
-                  </Link>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link  fw-medium" to="testimonials">
+                  <NavLink
+                    className="nav-link  fw-medium"
+                    to="testimonials"
+                    onClick={closeNavbar}
+                  >
                     Testimonials
-                  </Link>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link  fw-medium" to="blog">
+                  <NavLink
+                    className="nav-link  fw-medium"
+                    to="blog"
+                    onClick={closeNavbar}
+                  >
                     Blog
-                  </Link>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link  fw-medium" to="contact-us">
+                  <NavLink
+                    className="nav-link  fw-medium"
+                    to="contact-us"
+                    onClick={closeNavbar}
+                  >
                     Contact Us
-                  </Link>
+                  </NavLink>
                 </li>
                 <div className="d-flex gap-1 navbar-contact">
-                <li className=" d-flex nav-link bg-white rounded-5 fw-medium justify-content-center align-items-center">
-                  <p className="p-0 m-0">0797578699</p>
-                </li>
-                <li className="nav-item px-1">
-                  <a
-                    href="https://www.facebook.com/mrkamin2"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="footer-socials rounded-5"
-                  >
-                    <FaFacebook />
-                  </a>
-                </li>
+                  <li className=" d-flex nav-link bg-white rounded-5 fw-medium justify-content-center align-items-center">
+                    <p className="p-0 m-0">0797578699</p>
+                  </li>
+                  <li className="nav-item px-1">
+                    <a
+                      href="https://www.facebook.com/mrkamin2"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="footer-socials rounded-5"
+                    >
+                      <FaFacebook />
+                    </a>
+                  </li>
                 </div>
               </ul>
             </div>
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,148 +1,248 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import NavUperHead from "./NavUperHead";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import StarSvg from "./StarSvg";
+import { FaFacebook } from "react-icons/fa";
 import IMG from "../assets/Logo2.png";
 
 const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleNavbarToggle = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  const closeNavbar = () => {
+    setNavbarOpen(false);
+  };
+
   return (
     <>
       <div className="header-navbar d-flex flex-column-reverse">
         <nav className="navbar navbar-expand-lg p-0 m-0">
           <div className="container-fluid">
-            <Link className="navbar-brand rounded-3" to="/">
+            <NavLink
+              className="navbar-brand rounded-3"
+              to="/"
+              onClick={closeNavbar}
+            >
               <img src={IMG} alt="Logo" height={80} />
-            </Link>
+            </NavLink>
             <button
               className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
+              onClick={handleNavbarToggle}
             >
               <span className="navbar-toggler-icon"></span>
             </button>
             <div
-              className="collapse navbar-collapse"
+              className={`collapse navbar-collapse ${navbarOpen ? "show" : ""}`}
               id="navbarSupportedContent"
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item dropdown">
                   <div className="d-flex">
-                    <Link
+                    <NavLink
                       className="nav-link fw-medium"
                       to="air-conditioning"
+                      onClick={closeNavbar}
                     >
                       Air-Conditioning
-                    </Link>
-                    <Link
+                    </NavLink>
+                    <NavLink
                       className="nav-link dropdown-toggle fw-medium"
                       role="button"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
-                    ></Link>
+                    ></NavLink>
                     <ul class="dropdown-menu">
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Domestic Split Installation Offer
-                        </Link>
+                        </NavLink>
                       </li>
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Residential Installation
-                        </Link>
+                        </NavLink>
                       </li>
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Maintenance & Service
-                        </Link>
+                        </NavLink>
                       </li>
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Repair
-                        </Link>
+                        </NavLink>
                       </li>
                     </ul>
                   </div>
                 </li>
                 <li className="nav-item dropdown">
                   <div className="d-flex">
-                    <Link
+                    <NavLink
                       className="nav-link fw-medium"
                       to="commercial-refrigeration"
+                      onClick={closeNavbar}
                     >
                       Commercial Refrigeration
-                    </Link>
-                    <Link
+                    </NavLink>
+                    <NavLink
                       className="nav-link dropdown-toggle fw-medium"
                       role="button"
                       data-bs-toggle="dropdown"
                       aria-expanded="false"
-                    ></Link>
+                    ></NavLink>
                     <ul class="dropdown-menu">
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Cold Rooms & Cellar Cooler
-                        </Link>
+                        </NavLink>
                       </li>
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Maintenance & Service
-                        </Link>
+                        </NavLink>
                       </li>
                       <li>
-                        <Link
+                        <NavLink
                           class="dropdown-item"
                           to="commercial-refrigeration"
+                          onClick={closeNavbar}
                         >
                           Repair
-                        </Link>
+                        </NavLink>
                       </li>
                     </ul>
                   </div>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link  fw-medium" to="repair-maintenance">
+                  <NavLink
+                    className="nav-link  fw-medium"
+                    to="repair-maintenance"
+                    onClick={closeNavbar}
+                  >
                     Repair and Maintenance
-                  </Link>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link className="nav-link  fw-medium" to="our-projects">
+                  <NavLink
+                    className="nav-link  fw-medium"
+                    to="our-projects"
+                    onClick={closeNavbar}
+                  >
                     Our Projects
-                  </Link>
+                  </NavLink>
                 </li>
                 <li className="nav-item">
-                  <Link
+                  <NavLink
                     className="support nav-link bg-white rounded-5 fw-medium text-center"
+                    onClick={closeNavbar}
                     to="contactus"
                   >
                     24/7 Support
-                  </Link>
+                  </NavLink>
                 </li>
               </ul>
             </div>
           </div>
         </nav>
-        <NavUperHead />
+        <div className="d-flex justify-content-end">
+          {" "}
+          <nav className="navbar navbar-expand-lg p-0 m-0">
+            <div className="container-fluid">
+              <div
+                className={`collapse navbar-collapse ${
+                  navbarOpen ? "show" : ""
+                }`}
+                id="navbarSupportedContent"
+              >
+                <div className="d-flex align-items-center review">
+                  <StarSvg />
+                  <p className="p-0 m-0">Review</p>
+                </div>
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link  fw-medium"
+                      aria-current="page"
+                      to="about-us"
+                      onClick={closeNavbar}
+                    >
+                      About Us
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link  fw-medium"
+                      to="testimonials"
+                      onClick={closeNavbar}
+                    >
+                      Testimonials
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link  fw-medium"
+                      to="blog"
+                      onClick={closeNavbar}
+                    >
+                      Blog
+                    </NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink
+                      className="nav-link  fw-medium"
+                      to="contact-us"
+                      onClick={closeNavbar}
+                    >
+                      Contact Us
+                    </NavLink>
+                  </li>
+                  <div className="d-flex gap-1 navbar-contact">
+                    <li className=" d-flex nav-link bg-white rounded-5 fw-medium justify-content-center align-items-center">
+                      <p className="p-0 m-0">0797578699</p>
+                    </li>
+                    <li className="nav-item px-1">
+                      <a
+                        href="https://www.facebook.com/mrkamin2"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="footer-socials rounded-5"
+                      >
+                        <FaFacebook />
+                      </a>
+                    </li>
+                  </div>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
       </div>
     </>
   );
