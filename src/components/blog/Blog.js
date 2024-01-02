bf0c68de5e7ed2e586401ad5fbf5d273b7ef48a6
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-01-02 15:06:00 +0500

    Make sanity fucntional

--- src/components/blog/Blog.js
+++ src/components/blog/Blog.js
@@ -1,33 +1,58 @@
-import IMG1 from '../assets/airconditioning/1.jpg';
-import IMG2 from '../assets/airconditioning/2.jpg';
-import IMG3 from '../assets/airconditioning/3.jpg';
-import IMG4 from '../assets/airconditioning/4.jpg';
-import IMG5 from '../assets/airconditioning/5.avif';
-import IMG6 from '../assets/airconditioning/6.jpg';
-import IMG7 from '../assets/airconditioning/8.avif';
-import IMG8 from '../assets/airconditioning/7.webp';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import sanityClient from '../client';
 
-const Blog = () => (
-  <div className="py-5">
-    <div className="container text-center">
-      <h1>Air conditioning & Refrigeration Blog</h1>
-      <p>
-        Stories, News and Information about Air conditioning and refrigeration.
-        <br />
-        Learn more about the products and services we offer
-      </p>
-    </div>
-    <div className="air-cond-projects">
-      <img src={IMG1} alt="IMG1" height={200} />
-      <img src={IMG2} alt="IMG2" height={200} />
-      <img src={IMG3} alt="IMG3" height={200} />
-      <img src={IMG4} alt="IMG4" height={200} />
-      <img src={IMG5} alt="IMG5" height={200} />
-      <img src={IMG6} alt="IMG6" height={200} />
-      <img src={IMG7} alt="IMG7" height={200} />
-      <img src={IMG8} alt="IMG8" height={200} />
-    </div>
-  </div>
-);
+const Blog = () => {
+  const [projects, setProjects] = useState(null);
+
+  useEffect(() => {
+    sanityClient.fetch(
+      `*[_type == "post"]{
+  title,
+  slug,
+  mainImage{
+    asset->{
+      _id,
+      url
+    },
+    alt
+  }
+}`,
+    )
+      .then((data) => setProjects(data))
+      .catch(console.error);
+  });
+  return (
+    <main>
+      <section>
+        <h1>Air conditioning & Refrigeration Blog</h1>
+        <p>
+          Stories, News and Information about Air conditioning and
+          refrigeration.
+          <br />
+          Learn more about the products and services we offer
+        </p>
+        <div>
+          {projects && projects.map((post) => (
+            <article key={post.slug.current}>
+              <Link to={`/post/${post.slug.current}`} key={post.slug.currect}>
+                <span className="">
+                  <img
+                    src={post.mainImage.asset.url}
+                    alt={post.mainImage.alt}
+                    className=""
+                  />
+                  <span>
+                    <h3>{post.title}</h3>
+                  </span>
+                </span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      </section>
+    </main>
+  );
+};
 
 export default Blog;
